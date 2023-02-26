052485e4e4e60932a43b776a168bf2d4d247b4db
Mbeex <maja.obuchowska@o2.pl>
2023-02-26 21:34:43 +0100

    Zadanie dzien4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Zadanie dzien4
+
+string name = "Ewa";
+int age = 33;
+bool kobieta = true;
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+if (age < 18 && kobieta == false)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+if (kobieta == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Mężczyzna powyżej 30 lat");
+}
+
+
