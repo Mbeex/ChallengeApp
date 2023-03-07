32f95e1d78cfe232c456c90f166c92256b22b361
Mbeex <maja.obuchowska@o2.pl>
2023-03-07 20:22:48 +0100

    Praca domowa dzien6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,69 +1,47 @@
-﻿//Praca domowa dzien 5
+﻿using ChallengeApp;
 
-int number = 922374;
-string numberinString = number.ToString();
-char[] letters = numberinString.ToArray();
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+var employee1 = new Employee("Maja", " Obuchowska", 18);
+var employee2 = new Employee("Darek", " Nowak", 24);
+var employee3 = new Employee("Alicja", " Kowal", 20);
 
-foreach (char letter in letters)
+employee1.AddScore(8);
+employee1.AddScore(7);
+employee1.AddScore(9);
+employee1.AddScore(7);
+employee1.AddScore(8);
 
-    if (letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
+employee2.AddScore(4);
+employee2.AddScore(2);
+employee2.AddScore(5);
+employee2.AddScore(1);
+employee2.AddScore(7);
 
-Console.WriteLine("0=>" + counter0);
-Console.WriteLine("1=>" + counter1);
-Console.WriteLine("2=>" + counter2);
-Console.WriteLine("3=>" + counter3);
-Console.WriteLine("4=>" + counter4);
-Console.WriteLine("5=>" + counter5);
-Console.WriteLine("6=>" + counter6);
-Console.WriteLine("7=>" + counter7);
-Console.WriteLine("8=>" + counter8);
-Console.WriteLine("9=>" + counter9);
+employee3.AddScore(5);
+employee3.AddScore(5);
+employee3.AddScore(5);
+employee3.AddScore(8);
+employee3.AddScore(2);
+
+int result1 = employee1.Result;
+int result2 = employee2.Result;
+int result3 = employee3.Result;
+
+
+List<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach(var employee in employees)
+{
+    if(employee.Result > maxResult)
+    {
+        employeeWithMaxResult = employee;
+        maxResult = employeeWithMaxResult.Result;
+    }
+}
+
+Console.WriteLine("Najlepszy pracownik to " +  employeeWithMaxResult.Name +  employeeWithMaxResult.Surname + " lat " +  employeeWithMaxResult.Age + " ktory zdobyl " + maxResult + " punktow!!");
