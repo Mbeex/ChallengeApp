0298d863005cda2c1d52fdd1763d601e6a66189e
Mbeex <maja.obuchowska@o2.pl>
2023-03-22 21:09:46 +0100

    dzien11

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,13 +1,42 @@
 ﻿using ChallengeApp;
 
-var employee = new Employee("Maja","Obuchowska");
-employee.AddGrade("Maja");
-employee.AddGrade("4000");
-employee.AddGrade(2);
-employee.AddGrade(6);
-var statistics = employee.GetStatistics();
+var employee1 = new Employee("Maja","Obuchowska");
+var employee2 = new Employee("Konrad", "Nowak");
+var employee3 = new Employee("Justyna", "Kowal");
+var employee4 = new Employee("Karol", "Kowalski");
 
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
+
+employee1.AddGrade("seven");
+employee1.AddGrade("11");
+employee1.AddGrade(2);
+employee1.AddGrade(6);
+
+employee2.AddGrade("2.5");
+employee2.AddGrade(8);
+employee2.AddGrade(1000);
+employee2.AddGrade(2);
+
+employee3.AddGrade(6);
+employee3.AddGrade(7);
+employee3.AddGrade(2);
+employee3.AddGrade(2);
+
+employee4.AddGrade(3);
+employee4.AddGrade(12);
+employee4.AddGrade(1);
+employee4.AddGrade(2);
+
+var statistics1 = employee1.GetStatisticsWithForeach();
+var statistics2 = employee2.GetStatisticsWithFor();
+var statistics3 = employee3.GetStatisticsWithDoWhile();
+var statistics4 = employee4.GetStatisticsWithWhile();
+
+Console.WriteLine($"Foreach:\nEmployee1:\nAverage: {statistics1.Average:N2}\nMax: {statistics1.Max}\nMin: {statistics1.Min}");
+Console.WriteLine($"For:\nEmployee2:\nAverage: {statistics2.Average:N2}\nMax: {statistics2.Max}\nMin: {statistics2.Min}");
+Console.WriteLine($"DoWhile:\nEmployee3:\nAverage: {statistics3.Average:N2}\nMax: {statistics3.Max}\nMin: {statistics3.Min}");
+Console.WriteLine($"While:\nEmployee4:\nAverage: {statistics4.Average:N2}\nMax: {statistics4.Max}\nMin: {statistics4.Min}");
+
+
+
+
 
