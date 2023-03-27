e8d54ff59a7b8c3e00fd2d90a1b42f1e1f2c16ca
Mbeex <maja.obuchowska@o2.pl>
2023-03-27 20:16:09 +0200

    dzien12

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -26,17 +26,8 @@ employee4.AddGrade(12);
 employee4.AddGrade(1);
 employee4.AddGrade(2);
 
-var statistics1 = employee1.GetStatisticsWithForeach();
-var statistics2 = employee2.GetStatisticsWithFor();
-var statistics3 = employee3.GetStatisticsWithDoWhile();
-var statistics4 = employee4.GetStatisticsWithWhile();
-
-Console.WriteLine($"Foreach:\nEmployee1:\nAverage: {statistics1.Average:N2}\nMax: {statistics1.Max}\nMin: {statistics1.Min}");
-Console.WriteLine($"For:\nEmployee2:\nAverage: {statistics2.Average:N2}\nMax: {statistics2.Max}\nMin: {statistics2.Min}");
-Console.WriteLine($"DoWhile:\nEmployee3:\nAverage: {statistics3.Average:N2}\nMax: {statistics3.Max}\nMin: {statistics3.Min}");
-Console.WriteLine($"While:\nEmployee4:\nAverage: {statistics4.Average:N2}\nMax: {statistics4.Max}\nMin: {statistics4.Min}");
-
-
-
-
+var statistics1 = employee1.GetStatistics();
+var statistics2 = employee2.GetStatistics();
+var statistics3 = employee3.GetStatistics();
+var statistics4 = employee4.GetStatistics();
 
